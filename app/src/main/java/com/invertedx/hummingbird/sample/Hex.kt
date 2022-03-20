524b74d685686781872de084987ebce8eebc0214
Sarath <sarath@samourai.io>
2022-03-20 23:33:23 +0530

    Sample: add QR Activity

---
+++ app/src/main/java/com/invertedx/hummingbird/sample/Hex.kt
@@ -0,0 +1,25 @@
+package com.invertedx.hummingbird.sample
+
+
+fun hexToBytes(s: String): ByteArray? {
+    val len = s.length
+    val data = ByteArray(len / 2)
+    var i = 0
+    while (i < len) {
+        data[i / 2] = ((Character.digit(s[i], 16) shl 4)
+                + Character.digit(s[i + 1], 16)).toByte()
+        i += 2
+    }
+    return data
+}
+
+private val HEX_ARRAY = "0123456789abcdef".toCharArray()
+fun bytesToHex(bytes: ByteArray): String {
+    val hexChars = CharArray(bytes.size * 2)
+    for (j in bytes.indices) {
+        val v: Int = bytes[j].toInt() and 0xFF
+        hexChars[j * 2] = HEX_ARRAY[v ushr 4]
+        hexChars[j * 2 + 1] = HEX_ARRAY[v and 0x0F]
+    }
+    return String(hexChars)
+}
--- app/src/main/java/com/invertedx/hummingbird/sample/QRActivity.kt
+++ app/src/main/java/com/invertedx/hummingbird/sample/QRActivity.kt
@@ -1,12 +1,53 @@
 package  com.invertedx.hummingbird.sample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.invertedx.hummingbird.URQRView
+import com.sparrowwallet.hummingbird.UR
+
 
 class QRActivity : AppCompatActivity() {
+
+    var type = 0;
+    var psbtHex = "70736274ff01009a020000000258e87a21b56daf0c23be8e7070456c336f7cbaa5c8757924f545887bb2abdd750000000000ffffffff838d0427d0ec650a68aa46bb0b098aea4422c071b2ca78352a077959d07cea1d0100000000ffffffff0270aaf00800000000160014d85c2b71d0060b09c9886aeb815e50991dda124d00e1f5050000000016001400aea9a2e5f0f876a588df5546e8742d1d87008f000000000000000000"
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qractivity)
+        val urView = findViewById<URQRView>(R.id.urView)
+        setSupportActionBar(findViewById(R.id.toolbarQR));
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "QR "
+        if (intent.extras?.containsKey("TYPE") == true) {
+            intent.extras?.getInt("TYPE")?.let {
+                type = it
+            }
+        }
+        urView.qrMargin = 1
+        val urFrameStatus = findViewById<TextView>(R.id.urFrameStatus);
+        if (type == 0) {
+            val hex = "Hello world"
+            urView.content = hex
+            findViewById<TextView>(R.id.qrContentText).text = urView.content
+        } else if (type == 1) {
+            urView.maxFragmentLength = 100
+            urView.setUrTransmissionListener { totalFrames, currentFrame ->
+                urFrameStatus.visibility = View.VISIBLE
+                urFrameStatus.text = "Current Frame: ${currentFrame} - TotalFrames ${totalFrames} \n\n FPS: ${urView.fps}"
+            }
+            urView.setContent(UR.fromBytes(hexToBytes(psbtHex)));
+            findViewById<TextView>(R.id.qrContentText).text =psbtHex
+        }
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
