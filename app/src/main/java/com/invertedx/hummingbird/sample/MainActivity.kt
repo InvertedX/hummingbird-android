bba8fd98013794f063025623a5a598dbca9125c7
Sarath <sarath@samourai.io>
2022-03-20 23:34:33 +0530

    Sample: add main screen for examples

--- app/src/main/java/com/invertedx/hummingbird/sample/MainActivity.kt
+++ app/src/main/java/com/invertedx/hummingbird/sample/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.invertedx.hummingbird.sample
 
+import android.Manifest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.invertedx.hummingbird.QRScanner
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.viewQR).setOnClickListener {
+      setSupportActionBar( findViewById(R.id.mainActivityToolBar))
+        findViewById<Button>(R.id.showQR).setOnClickListener {
             startActivity(
                 Intent(
                     this,
                     QRActivity::class.java
+                ).apply {
+                    putExtra("TYPE",0)
+                }
+            )
+        }
+        findViewById<Button>(R.id.showPSBTQR).setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    QRActivity::class.java
+                ).apply {
+                    putExtra("TYPE",1)
+                }
+            )
+        }
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            12
+        )
+//
+        findViewById<Button>(R.id.scanQRBtn).setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    ScanActivity::class.java
                 )
             )
         }
